5cd9616ffcc7e54f215c243a40de580e94f184ac
mtakle <martin.takle@gmail.com>
2018-09-04 13:47:19 +0200

    Added script for learning functions

---
+++ learning functions.py
@@ -0,0 +1,17 @@
+#learning functions
+
+def my_function():
+    print("Hello from a function")
+
+my_function()
+
+
+def my_function_2(fname):
+    print(fname + " is your input")
+
+my_function_2("AAAA")
+
+def my_function_3(x):
+    return 5 * x
+
+print(my_function_3(7))
